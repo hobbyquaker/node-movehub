207e775e91c995bf9ce8d191e1f2c0b207c982e4
hobbyquaker <bastiraff@googlemail.com>
2017-08-27 00:13:18 +0200

    add travis, gulp-xo

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,13 @@
 const gulp = require('gulp');
 const jsdoc2md = require('jsdoc-to-markdown');
+const xo = require('gulp-xo');
+
+gulp.task('lint', () =>
+    gulp.src('index.js')
+        .pipe(xo())
+        .pipe(xo.format())
+        .pipe(xo.failAfterError())
+);
 
 gulp.task('docs', function () {
     const fs = require('fs');
