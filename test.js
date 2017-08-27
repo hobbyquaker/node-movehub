3af78b661e17d60fa47c348087d5a4e0c9fd6356
hobbyquaker <bastiraff@googlemail.com>
2017-08-27 02:26:36 +0200

    tests

--- test.js
+++ test.js
@@ -1,7 +1,5 @@
 const boost = require('./index.js');
 
-
-
 boost.on('error', (err) => {
     console.error('boost error', err);
 });
@@ -14,18 +12,31 @@ boost.on('hub-found', data => {
     console.log('boost hub-found', data);
 });
 
-boost.on('connect', () => {
-    console.log('boost connect');
-    boost.led('red');
-
-    setTimeout(() => {
-        boost.motorAngle('A', 120, 100);
-    }, 1000);
-    setTimeout(() => {
-        boost.motorAngle('A', 120, -100);
-    }, 2000);
-});
-
 boost.on('disconnect', () => {
     console.log('boost disconnect');
+});
+
+boost.on('distance', distance => {
+    console.log('distance', distance, 'mm');
+});
+
+boost.on('color', color => {
+    console.log('color', color);
+});
+
+boost.on('port', (details) => {
+    console.log('port', '0x' + details.port.toString(16), '0x' + details.state.toString(16));
+});
+
+boost.on('connect', () => {
+    console.log('boost connect');
+    boost.subscribe(0x01);
+
+    boost.led('red');
+    boost.motorAngle('A', 180, 50);
+
+    setTimeout(() => {
+        boost.led('green');
+        boost.motorAngle('A', 180, -50);
+    }, 2000);
 });
